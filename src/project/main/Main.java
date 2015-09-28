0821cfea462aa0bb1388029536c64b7cc38181df
Jamesyd91 <sarah@hotmail.com>
2015-09-28 12:25:08 +0100

    Scanner test
    Added scanner with greet.

--- src/project/main/Main.java
+++ src/project/main/Main.java
@@ -1,10 +1,20 @@
 package project.main;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Name: ");
+		
+		String name = sc.nextLine();
+		
+		System.out.println("Hello " + name);
 	}
 
 }
