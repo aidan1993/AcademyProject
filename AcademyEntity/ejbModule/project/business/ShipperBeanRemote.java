3a4753babbbc988168eaf8fb89557dac15801746
aidan1993 <aidan.mcaleese93@gmail.com>
2015-09-29 16:21:56 +0100

    EJB stuff added - not working yet

---
+++ AcademyEntity/ejbModule/project/business/ShipperBeanRemote.java
@@ -0,0 +1,15 @@
+package project.business;
+
+import java.util.List;
+
+import javax.ejb.Remote;
+
+import project.entity.Shipper;
+
+@Remote
+public interface ShipperBeanRemote {
+	void saveShipper(Shipper s);
+	void deleteShipper(Shipper s);
+	Shipper findShipper(Shipper s);
+	List<Shipper> retrieveAllShippers();
+}
