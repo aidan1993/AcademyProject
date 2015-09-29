76e5f9c691d9be1b3d3f67ad04ad8a9fa45f4a4c
aidan1993 <aidan.mcaleese93@gmail.com>
2015-09-29 12:35:46 +0100

    SQL Script updated

--- SQL script for academy project.sql
+++ SQL script for academy project.sql
@@ -5,12 +5,12 @@ use AcademyProject;
 Create table Stocks (
 	Stockid INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
 	StockSymbol nvarchar(10) NOT NULL, 
-	AskPrice Double NOT NULL, 
-	AskMAvg Double, 
-	BidPrice Double NOT NULL, 
-	BidMAvg Double, 
-	TodaysOpen Double NOT NULL, 
-	PreviousClose Double NOT NULL, 
+    BidPrice DECIMAL(6,2) NOT NULL, 
+	BidMAvg DECIMAL(6,2), 
+	AskPrice DECIMAL(6,2) NOT NULL, 
+	AskMAvg DECIMAL(6,2),
+	TodaysOpen DECIMAL(6,2) NOT NULL, 
+	PreviousClose DECIMAL(6,2) NOT NULL, 
 	Time_Of timestamp NOT NULL
 );
 
@@ -19,12 +19,12 @@ Create table Transactions (
     Stockid INT NOT NULL,
 	StockSymbol nvarchar(20) NOT NULL, 
 	Volume int NOT NULL, 
-	Buy double NOT NULL,
+	Buy DECIMAL(6,2) NOT NULL,
     BuyTime Timestamp NOT NULL,
-	Sell double NULL,
+	Sell DECIMAL(6,2) NULL,
     SellTime Timestamp NULL,
-	CurrentPosition SET('Open', 'Close') NOT NULL,
-    FOREIGN KEY(Stockid) REFERENCES Stocks(Stockid)
+	CurrentPosition ENUM('Open', 'Close') DEFAULT 'Open' NOT NULL,
+    FOREIGN KEY(Stockid) REFERENCES Stocks(Stockid) ON DELETE CASCADE
 );
 
 select * from Stocks;
