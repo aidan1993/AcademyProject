78c36bad6cfd0d8e84526f3d41fe7a7d2aa76361
Jamesyd91 <jamesy_d@hotmail.co.uk>
2015-09-29 10:15:10 +0100

    Added Connection
    connected to sql test.

--- src/project/main/GetLiveFeed.java
+++ src/project/main/GetLiveFeed.java
@@ -1,7 +1,23 @@
 package project.main;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class GetLiveFeed {
 	
-	
+	public static void getConnection() {
+		Connection cn = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			cn = DriverManager.getConnection(
+					"jdbc:mysql://localhost/DATABASENAME", "root", "password");
+		} catch (SQLException ex) {
+			System.out.println("Database connection error: " + ex);
+		} catch (ClassNotFoundException ex) {
+			System.out.println("Class not found" + ex);
+		}
+
+	}
 
 }
