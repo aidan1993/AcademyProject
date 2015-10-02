ed992bbfd2c0bc721053c8672b759eb0f78c3d0d
aidan1993 <aidan.mcaleese93@gmail.com>
2015-10-02 09:08:00 +0100

    clean project set up

--- AcademyWeb/src/project/servlets/DatabaseServlet.java
+++ AcademyWeb/src/project/servlets/DatabaseServlet.java
@@ -2,12 +2,10 @@ package project.servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.ejb.EJB;
 import javax.naming.InitialContext;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
